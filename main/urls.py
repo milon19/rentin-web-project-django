c8b7058c162037818d00fba7e138122c7980bac1
milon19 <milon15-1472@diu.edu.bd>
2020-04-05 15:29:35 +0600

    product-list-template

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import Home
+from .views import Home, ProductListView
 
 urlpatterns = [
     path('', Home, name='home'),
+    path('products/', ProductListView, name='products'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse
 
 def Home(request):
     return render(request, 'main/home.html')
+
+
+def ProductListView(request):
+    return render(request, 'main/products.html')
